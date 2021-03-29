6633453278229e8681a9e823de062003886f255a
Angel Hurtado <contact@angelxehg.com>
2021-03-29 15:25:47 -0600

    Crear componente Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link, NavLink } from 'react-router-dom'
+
+export const Navbar = props => (
+  <nav className='navbar navbar-expand-sm navbar-light bg-light'>
+    <div className='container-md'>{props.children}</div>
+  </nav>
+)
+
+Navbar.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export const NavbarBrand = props => (
+  <Link className='navbar-brand' to={props.to}>
+    {props.title}
+  </Link>
+)
+
+NavbarBrand.propTypes = {
+  title: PropTypes.string.isRequired,
+  to: PropTypes.string.isRequired,
+}
+
+export const NavbarToggler = () => (
+  <button
+    className='navbar-toggler'
+    type='button'
+    data-bs-toggle='collapse'
+    data-bs-target='#navbarSupportedContent'
+    aria-controls='navbarSupportedContent'
+    aria-expanded='false'
+    aria-label='Toggle navigation'
+  >
+    <span className='navbar-toggler-icon'></span>
+  </button>
+)
+
+export const NavbarCollapse = props => (
+  <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+    <ul className='navbar-nav'>{props.children}</ul>
+  </div>
+)
+
+NavbarCollapse.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export const NavbarItem = props => (
+  <li className='nav-item'>
+    <NavLink className='nav-link' to={props.to} activeClassName='active'>
+      {props.title}
+    </NavLink>
+  </li>
+)
+
+NavbarItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  to: PropTypes.string.isRequired,
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 
+import { Navbar, NavbarBrand, NavbarCollapse, NavbarItem, NavbarToggler } from "../components/Navbar"
+
 const HomePage = () => {
   return (
-    <div className="container pt-2 pb-2">
-      <header>
-        <h1>React + Capacitor Examples</h1>
-        <p>Hola mundo</p>
-      </header>
+    <div>
+      <Navbar>
+        <NavbarBrand title="React + Capacitor" to="/" />
+        <NavbarToggler />
+        <NavbarCollapse>
+          <NavbarItem title="Inicio" to="/home" />
+        </NavbarCollapse>
+      </Navbar>
+      <div className="container pt-2 pb-2">
+        <header>
+          <h1>React + Capacitor Examples</h1>
+          <p>Hola mundo</p>
+        </header>
+      </div>
     </div>
   )
 }
