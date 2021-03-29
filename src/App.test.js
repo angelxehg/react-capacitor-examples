90568f6e16873fe075f2da38bf2cacc6083c4f50
Angel Hurtado <contact@angelxehg.com>
2021-03-29 14:43:28 -0600

    Eliminar plantilla inicial

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // const linkElement = screen.getByText(/React + Capacitor Examples/i);
+  // expect(linkElement).toBeInTheDocument();
 });
