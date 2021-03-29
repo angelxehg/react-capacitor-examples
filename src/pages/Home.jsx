a10ff598a1cbe7cfb8b4b638114cb591dadf8b09
Angel Hurtado <contact@angelxehg.com>
2021-03-29 16:49:19 -0600

    Mostrar links a contenidos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,45 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import DefaultNavbar from "../components/Navbar";
 import { Container } from '../layouts/Container';
+import { Column, Row } from '../layouts/Grid';
 
 const HomePage = () => {
   return (
     <div>
-      <DefaultNavbar/>
+      <DefaultNavbar />
       <Container className="pt-3 pb-3">
         <header>
-          <h1>React + Capacitor Examples</h1>
+          <h1>
+            React + Capacitor Examples
+            <span className="badge bg-secondary ms-2">v0.1.0</span>
+          </h1>
           <p>Ejemplos de uso de Capacitor en un proyecto con React</p>
         </header>
+        <main>
+          <h2>Contenidos</h2>
+          <Row>
+            <Column xl="3" lg="4" md="6" className="p-1" >
+              <div className="card">
+                <div className="card-body">
+                  <h3 className="card-title">Layouts</h3>
+                  <p className="card-text">Pruebas con layouts disponibles en Bootstrap</p>
+                  <Link to="/layouts">Ver ejemplos</Link>
+                </div>
+              </div>
+            </Column>
+            <Column xl="3" lg="4" md="6" className="p-1" >
+              <div className="card">
+                <div className="card-body">
+                  <h3 className="card-title">Plugins</h3>
+                  <p className="card-text">Pruebas con plugins disponibles en Capacitor</p>
+                  <Link to="/plugins">Ver ejemplos</Link>
+                </div>
+              </div>
+            </Column>
+          </Row>
+        </main>
       </Container>
     </div>
   )
